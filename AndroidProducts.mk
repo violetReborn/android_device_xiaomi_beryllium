94e8a3f598f5a0ef5689a2ebcdadf49a9e2f44c8
GoZiko <anirban89m@gmail.com>
2025-07-08 07:44:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/infinity_beryllium.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_beryllium-eng \
+    infinity_beryllium-userdebug \
+    infinity_beryllium-user
